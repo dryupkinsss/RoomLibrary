ab82b8a1cb9c9bca508e507ed41a9f58365d98d3
dryapkins <116013737+zxc-t0xic@users.noreply.github.com>
2023-02-08 03:38:27 +0300

    Добавьте файлы проекта.

---
+++ RoomLibrary/Room.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoomLibrary
+{
+    public class Room
+    {
+        double roomLength;  // длина комнаты
+        double roomWidth;   // ширина комнаты               
+        public double RoomLength
+        {
+            get { return roomLength; }
+            set { roomLength = value; }
+        }
+        public double RoomWifth
+        {
+            get { return roomWidth; }
+            set { roomWidth = value; }
+        }
+        /// <summary>
+        /// Метод вычисляет периметр комнаты
+        /// </summary>
+        /// <returns>Возвращает значение периметра</returns>
+        public double RoomPerimetr()
+        {
+            return 2 * (roomLength + roomWidth);
+        }
+        /// <summary>
+        /// метод вычисляет площадь комнаты
+        /// </summary>
+        /// <returns>Возвращает значение площади</returns>
+        public double RoomArea()
+        {
+            return roomLength * roomWidth;
+        }
+        /// <summary>
+        /// метод вычисляет число квадратных метров
+        /// на одного человека
+        /// </summary>
+        /// <param name="np">Число человек</param>
+        /// <returns>Возвращает число квадратных метров</returns>
+        public double PersonArea(int np)
+        {
+            return RoomArea() / np;
+        }
+    }
+}
